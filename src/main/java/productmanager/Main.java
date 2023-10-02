4bfaf0f709c33c34a53a6580b22c25cc6a820778
Marat Vakhitov <404@bk.ru>
2023-10-02 08:57:08 +0300

    TEST-2. Corrections by comments

--- src/main/java/productmanager/Main.java
+++ src/main/java/productmanager/Main.java
@@ -2,16 +2,5 @@ package productmanager;
 
 public class Main {
 
-    public static void main(String[] args) {
 
-
-        String a = null;
-        String b = "";
-
-        if (a == null && b == "") {
-            System.out.println("print");
-        }
-
-
-    }
 }
