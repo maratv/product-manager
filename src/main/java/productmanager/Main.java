7824861da4d0c272fb3f7b158ffbaa288a7a49eb
Marat Vakhitov <404@bk.ru>
2023-10-02 07:44:28 +0300

    TEST-2. added temp class

---
+++ src/main/java/productmanager/Main.java
@@ -0,0 +1,17 @@
+package productmanager;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        String a = null;
+        String b = "";
+
+        if (a == null && b == "") {
+            System.out.println("print");
+        }
+
+
+    }
+}
